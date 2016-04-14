5708cc23477b3221a566ed391c93b7ea006c20eb
Siddhant Rath <sid@tamu.edu>
2016-04-14 16:03:06 -0500

    Delete Installing ChefDK.sh

--- Installing ChefDK.sh
+++
@@ -1,7 +0,0 @@
-#Installing ChefDK
-## You must be inside a vagrant instance runnuing ubuntu 64
-
-sudo apt-get install curl --yes > /dev/null
-
-curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -c current -P chefdk
-
