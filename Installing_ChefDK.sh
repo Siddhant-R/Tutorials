e830f98150898a3c31fd85cdf4fd7aa7d4037d12
Siddhant Rath <sid@tamu.edu>
2016-04-18 16:39:56 -0500

    Installing Chef Server

--- Installing_ChefDK.sh
+++ Installing_ChefDK.sh
@@ -1,7 +1,12 @@
-#Installing ChefDK
+# Installing ChefDK
 ## You must be inside a vagrant instance runnuing ubuntu 64
 
 sudo apt-get install curl --yes > /dev/null
 
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -c current -P chefdk
+
+# Configuring a resource
+#   A Chef resource describes some piece of infrastructure, such as a file, a template,
+#   or a package. A Chef recipe is a file that groups related resources, such as 
+#   everything needed to configure a web server, database server, or a load balancer.
 
